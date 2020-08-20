a855608a9c3df90d5cbc9d428098485a62433a11
Frank Moreno <frankmoreno1993@gmail.com>
2020-08-19 19:36:34 -0500

    BlocBuilder is ready using useBloc

--- packages/flutter_hooks_bloc/lib/flutter_hooks_bloc.dart
+++ packages/flutter_hooks_bloc/lib/flutter_hooks_bloc.dart
@@ -4,3 +4,4 @@ export 'package:flutter_bloc/flutter_bloc.dart'
     hide MultiBlocListener, BlocListener, BlocBuilder, BlocConsumer;
 export 'package:flutter_hooks/flutter_hooks.dart';
 export 'src/bloc_hook.dart' show useBloc;
+export 'src/bloc_builder.dart';
---
+++ packages/flutter_hooks_bloc/lib/src/bloc_builder.dart
@@ -0,0 +1,32 @@
+import 'bloc_hook.dart';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' hide BlocBuilder;
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+mixin BlocBuilderInterface<C extends Cubit<S>, S> on CubitComposer<C> {
+  BlocWidgetBuilder<S> get builder;
+  BlocBuilderCondition<S> get buildWhen;
+}
+
+class BlocBuilder<C extends Cubit<S>, S> extends HookWidget
+    with CubitComposer<C>, BlocBuilderInterface<C, S> {
+  const BlocBuilder({
+    Key key,
+    this.cubit,
+    @required this.builder,
+    this.buildWhen,
+  })  : assert(builder != null),
+        super(key: key);
+
+  final C cubit;
+  final BlocWidgetBuilder<S> builder;
+  final BlocBuilderCondition<S> buildWhen;
+
+  @override
+  Widget build(BuildContext context) {
+    final _cubit =
+        useBloc<C, S>(cubit: cubit, buildWhen: buildWhen, allowRebuild: true);
+    return builder(context, _cubit.state);
+  }
+}
