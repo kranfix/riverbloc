61e3614ab5b9adbf07afadf725e9880c4836a521
Frank Moreno <frankmoreno1993@gmail.com>
2020-08-21 23:25:19 -0500

    exporting only BlocListenable and BlocListener

--- packages/flutter_hooks_bloc/lib/flutter_hooks_bloc.dart
+++ packages/flutter_hooks_bloc/lib/flutter_hooks_bloc.dart
@@ -4,6 +4,6 @@ export 'package:flutter_bloc/flutter_bloc.dart'
     hide MultiBlocListener, BlocListener, BlocBuilder, BlocConsumer;
 export 'src/bloc_hook.dart' show useBloc;
 export 'src/bloc_builder.dart';
-export 'src/bloc_listener.dart' hide BlocListenableBase;
+export 'src/bloc_listener.dart' show BlocListenable, BlocListener;
 export 'src/bloc_consumer.dart';
 export 'src/multi_bloc_listener.dart';
