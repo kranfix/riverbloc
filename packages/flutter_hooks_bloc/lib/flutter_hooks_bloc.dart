d66961c80cd45624be7c83382a99224e2156f4d8
Frank Moreno <frankmoreno1993@gmail.com>
2020-08-21 23:02:25 -0500

    unexporting flutter_hooks by default

--- packages/flutter_hooks_bloc/lib/flutter_hooks_bloc.dart
+++ packages/flutter_hooks_bloc/lib/flutter_hooks_bloc.dart
@@ -2,7 +2,6 @@ library flutter_hooks_bloc;
 
 export 'package:flutter_bloc/flutter_bloc.dart'
     hide MultiBlocListener, BlocListener, BlocBuilder, BlocConsumer;
-export 'package:flutter_hooks/flutter_hooks.dart';
 export 'src/bloc_hook.dart' show useBloc;
 export 'src/bloc_builder.dart';
 export 'src/bloc_listener.dart' hide BlocListenableBase;
