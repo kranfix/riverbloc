d820e0d677a80f6bfbab841df828e68f0d78cc64
Frank Moreno <frankmoreno1993@gmail.com>
2020-08-30 16:51:42 -0500

    refactor: BlocWidget.use is protected

--- packages/flutter_hooks_bloc/lib/src/bloc_hook.dart
+++ packages/flutter_hooks_bloc/lib/src/bloc_hook.dart
@@ -20,6 +20,7 @@ abstract class BlocWidget<S> extends HookWidget {
 
   final Cubit<S> cubit;
 
+  @protected
   C use<C extends Cubit<S>>() =>
       useBloc<C, S>(cubit: cubit, onEmitted: onStateEmitted);
 
