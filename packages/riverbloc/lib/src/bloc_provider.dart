cf34727ced35c6c86d3f8324c6e5a69e080b8584
Frank Moreno <frankmoreno1993@gmail.com>
2020-08-29 22:06:25 -0500

    riverbloc BlocStateProviderState

--- packages/riverbloc/lib/src/bloc_provider.dart
+++ packages/riverbloc/lib/src/bloc_provider.dart
@@ -1,64 +1,47 @@
-import 'package:riverpod/riverpod.dart';
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+
+// ignore: implementation_imports
 import 'package:riverpod/src/framework.dart';
 
-class BlocProvider<C extends Cubit<Object>>
-    extends AlwaysAliveProviderBase<C, C> {
-  BlocProvider(
-    Create<C, ProviderReference> create, {
-    String name,
-  }) : super(create, name);
-
-  /// {@macro riverpod.family}
-  static const family = BlocProviderFamilyBuilder();
-
-  /// {@macro riverpod.autoDispose}
-  static const autoDispose = AutoDisposeBlocProviderBuilder();
-
-  AlwaysAliveProviderBase<C, Object> _state;
+class BlocStateProviderState<S> extends ProviderStateBase<Cubit<S>, S> {
+  StreamSubscription<S> _subscription;
 
   @override
-  _BlocProviderState<C, C> createState() => _BlocProviderState();
-
-  //@override
-  //_BlocProviderState<C> createState() => _BlocProviderState();
-}
-
-/// Adds [state] to [BlocProvider.autoDispose].
-//extension BlocStateProviderX<C extends Cubit<S>, S>
-//    on StateNotifierProvider<C> {
-//  /// {@macro riverpod.statenotifierprovider.state.provider}
-//  BlocStateProvider<S> get state {
-//    _state ??= BlocStateProvider<S>._(this);
-//    return _state as StateNotifierStateProvider<Value>;
-//  }
-//}
-
-class _BlocProviderState<C extends Cubit<S>, S>
-    extends ProviderStateBase<C, S> {
-  @override
-  void valueChanged({C previous}) {
-    // TODO: implement valueChanged
+  void valueChanged({Cubit<S> previous}) {
+    assert(
+      createdValue != null,
+      'BlocProvider must return a non-null value',
+    );
+    if (createdValue != previous) {
+      if (_subscription != null) {
+        _unsubscribe();
+      }
+      _subscribe();
+    }
   }
-}
 
-/// {@template riverpod.streamprovider.family}
-/// A class that allows building a [StreamProvider] from an external parameter.
-/// {@endtemplate}
-class BlocProviderFamily<C extends Cubit<Object>, A>
-    extends Family<C, AsyncValue<T>, A, ProviderReference, BlocProvider<C>> {
-  /// {@macro riverpod.streamprovider.family}
-  BlocProviderFamily(
-    C Function(ProviderReference ref, A a) create, {
-    String name,
-  }) : super(create, name);
+  void _subscribe() {
+    if (createdValue != null) {
+      _subscription = createdValue.listen(_listener);
+    }
+  }
+
+  void _unsubscribe() {
+    if (_subscription != null) {
+      _subscription.cancel();
+      _subscription = null;
+    }
+  }
+
+  void _listener(S value) {
+    exposedValue = value;
+  }
 
   @override
-  BlocProvider<C> create(
-    A value,
-    Cubit<C> Function(ProviderReference ref, A param) builder,
-    String name,
-  ) {
-    return StreamProvider((ref) => builder(ref, value), name: name);
+  void dispose() {
+    _unsubscribe();
+    super.dispose();
   }
 }
