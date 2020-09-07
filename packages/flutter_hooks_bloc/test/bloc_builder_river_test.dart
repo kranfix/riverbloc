9225af8a91646472119fd66d335a5c79fe391539
Frank Moreno <frankmoreno1993@gmail.com>
2020-09-07 11:33:18 -0500

    BlocBuilder.river passes initial state to widget

--- packages/flutter_hooks_bloc/test/bloc_builder_river_test.dart
+++ packages/flutter_hooks_bloc/test/bloc_builder_river_test.dart
@@ -14,6 +14,99 @@ class CounterCubit extends Cubit<int> {
 
 final counterProvider = BlocProvider((ref) => CounterCubit());
 
+class ThemeCubit extends Cubit<ThemeData> {
+  ThemeCubit() : super(ThemeData.light());
+
+  void setDarkTheme() => emit(ThemeData.dark());
+  void setLightTheme() => emit(ThemeData.light());
+}
+
+class DarkThemeCubit extends Cubit<ThemeData> {
+  DarkThemeCubit() : super(ThemeData.dark());
+
+  void setDarkTheme() => emit(ThemeData.dark());
+  void setLightTheme() => emit(ThemeData.light());
+}
+
+class MyThemeApp extends StatefulWidget {
+  MyThemeApp({
+    Key key,
+    @required Cubit<ThemeData> themeCubit,
+    @required Function onBuild,
+  })  : _themeCubit = themeCubit,
+        _onBuild = onBuild,
+        super(key: key);
+
+  final Cubit<ThemeData> _themeCubit;
+  final Function _onBuild;
+
+  @override
+  State<MyThemeApp> createState() => MyThemeAppState(
+        themeCubit: _themeCubit,
+        onBuild: _onBuild,
+      );
+}
+
+class MyThemeAppState extends State<MyThemeApp> {
+  MyThemeAppState({
+    @required Cubit<ThemeData> themeCubit,
+    @required Function onBuild,
+  })  : _themeCubit = themeCubit,
+        _onBuild = onBuild;
+
+  Cubit<ThemeData> _themeCubit;
+  bool change = false;
+  final Function _onBuild;
+
+  BlocProvider<Cubit<ThemeData>> themeProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    themeProvider = BlocProvider((ref) {
+      if (change) {
+        _themeCubit =
+            _themeCubit is ThemeCubit ? DarkThemeCubit() : ThemeCubit();
+      }
+      return _themeCubit;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: BlocBuilder<Cubit<ThemeData>, ThemeData>.river(
+        provider: themeProvider,
+        builder: ((context, theme) {
+          _onBuild();
+          return MaterialApp(
+            key: const Key('material_app'),
+            theme: theme,
+            home: Column(
+              children: [
+                RaisedButton(
+                  key: const Key('raised_button_1'),
+                  onPressed: () {
+                    change = true;
+                    context.refresh(themeProvider);
+                  },
+                ),
+                RaisedButton(
+                  key: const Key('raised_button_2'),
+                  onPressed: () {
+                    change = false;
+                    context.refresh(themeProvider);
+                  },
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
 void main() {
   group('BlocBuilder.river', () {
     testWidgets('throws if initialized with null cubit and builder',
@@ -44,6 +137,21 @@ void main() {
       } on dynamic catch (error) {
         expect(error, isAssertionError);
       }
+    });
+
+    testWidgets('passes initial state to widget', (tester) async {
+      final themeCubit = ThemeCubit();
+      var numBuilds = 0;
+      await tester.pumpWidget(
+        MyThemeApp(themeCubit: themeCubit, onBuild: () => numBuilds++),
+      );
+
+      final materialApp = tester.widget<MaterialApp>(
+        find.byKey(const Key('material_app')),
+      );
+
+      expect(materialApp.theme, ThemeData.light());
+      expect(numBuilds, 1);
     });
   });
 }
