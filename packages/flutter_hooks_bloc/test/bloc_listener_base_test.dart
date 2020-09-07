4098bf5af09398f2d21aa9abf4d93a20d1fb18a7
Frank Moreno <frankmoreno1993@gmail.com>
2020-09-07 13:02:48 -0500

    test for BlocListenerBase.river()

--- packages/flutter_hooks_bloc/test/bloc_listener_base_test.dart
+++ packages/flutter_hooks_bloc/test/bloc_listener_base_test.dart
@@ -4,8 +4,20 @@ import 'package:flutter_hooks_bloc/src/bloc_listener.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:riverbloc/riverbloc.dart';
 
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+}
+
 class MyBlocListener<C extends Cubit<S>, S> extends BlocListenerBase<C, S> {
   const MyBlocListener() : super(listener: null);
+
+  const MyBlocListener.river({
+    BlocProvider<C> provider,
+    BlocWidgetListener<S> listener,
+  }) : super.river(provider: provider, listener: listener);
 }
 
 void main() {
@@ -13,6 +25,31 @@ void main() {
     test('throws when super.listener is null', () {
       try {
         MyBlocListener();
+      } catch (e) {
+        expect(e, isAssertionError);
+      }
+    });
+  });
+
+  group('BlocListenerBase.river', () {
+    test('throws when super.provider is null', () {
+      try {
+        MyBlocListener.river(
+          provider: null,
+          listener: null,
+        );
+      } catch (e) {
+        expect(e, isAssertionError);
+      }
+    });
+
+    test('throws when super.provider is null', () {
+      final counterProvider = BlocProvider((ref) => CounterCubit());
+      try {
+        MyBlocListener.river(
+          provider: counterProvider,
+          listener: null,
+        );
       } catch (e) {
         expect(e, isAssertionError);
       }
