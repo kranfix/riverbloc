8c3ce4e081b50e3bbe9c4bebd9b394ffec109c39
Frank Moreno <frankmoreno1993@gmail.com>
2020-09-07 13:37:58 -0500

    BlocConsumer.river throws if builder is null

--- packages/flutter_hooks_bloc/test/bloc_consumer_river_test.dart
+++ packages/flutter_hooks_bloc/test/bloc_consumer_river_test.dart
@@ -9,6 +9,8 @@ class CounterCubit extends Cubit<int> {
   void increment() => emit(state + 1);
 }
 
+final counterProvider = BlocProvider((ref) => CounterCubit());
+
 void main() {
   group('BlocConsumer.river', () {
     testWidgets('throws AssertionError if provider is null', (tester) async {
@@ -26,12 +28,25 @@ void main() {
     });
 
     testWidgets('throws AssertionError if listener is null', (tester) async {
-      final counterProvider = BlocProvider((ref) => CounterCubit());
       try {
         await tester.pumpWidget(
           BlocConsumer<CounterCubit, int>.river(
             provider: counterProvider,
             listener: null,
+            builder: null,
+          ),
+        );
+      } on dynamic catch (error) {
+        expect(error, isAssertionError);
+      }
+    });
+
+    testWidgets('throws AssertionError if builder is null', (tester) async {
+      try {
+        await tester.pumpWidget(
+          BlocConsumer<CounterCubit, int>.river(
+            provider: counterProvider,
+            listener: (_, __) {},
             builder: null,
           ),
         );
