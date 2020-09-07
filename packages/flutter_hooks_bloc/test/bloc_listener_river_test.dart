3821771e62aa8db5d3247d63d587a8e3c91dd3fb
Frank Moreno <frankmoreno1993@gmail.com>
2020-09-06 20:26:38 -0500

    Preparing BlocListener.river test

---
+++ packages/flutter_hooks_bloc/test/bloc_listener_river_test.dart
@@ -0,0 +1,54 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_hooks_bloc/flutter_hooks_bloc.dart' hide BlocProvider;
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverbloc/riverbloc.dart' show BlocProvider;
+import 'package:flutter_test/flutter_test.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit() : super(0);
+
+  void increment() => emit(state + 1);
+}
+
+final counterProvider = BlocProvider((ref) => CounterCubit());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key, this.onListenerCalled}) : super(key: key);
+
+  final BlocWidgetListener<int> onListenerCalled;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: HookBuilder(
+          builder: (context) {
+            useRiverBloc<CounterCubit, int>(
+              counterProvider,
+              onEmitted: (context, _, state) {
+                onListenerCalled?.call(context, state);
+                return false;
+              },
+            );
+            return Column(
+              children: [
+                RaisedButton(
+                  key: const Key('cubit_listener_reset_button'),
+                  onPressed: () {
+                    context.refresh(counterProvider);
+                  },
+                ),
+                RaisedButton(
+                  key: const Key('cubit_listener_increment_button'),
+                  onPressed: () => context.read(counterProvider).increment(),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
