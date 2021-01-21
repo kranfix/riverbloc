d067b46808112adfddabc185f551ed638b7f9264
Christian Meunier <christian.meunier@ariane-software.com>
2021-01-22 05:29:46 +0800

    Add specific  entrypoint for usage with Riverbloc / Riverpod
    This library entrypoint does not explicitly import flutter_block or it's hook and therefore its BuildContext extensions that compete with riverpod's ones.

---
+++ packages/flutter_hooks_bloc/lib/flutter_hooks_riverbloc.dart
@@ -0,0 +1,7 @@
+library flutter_hooks_bloc;
+
+export 'src/bloc_hook.dart' show useRiverBloc;
+export 'src/bloc_builder.dart';
+export 'src/bloc_listener.dart' show BlocListener, BlocWidgetListener, BlocListenerCondition;
+export 'src/bloc_consumer.dart';
+export 'src/multi_bloc_listener.dart';
