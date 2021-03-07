ae782d7c947983724e7c6e301acb3354845dfb69
Frank Moreno <frankmoreno1993@gmail.com>
2021-03-06 20:55:10 -0500

    riverbloc: first 2 test for bloc

--- packages/riverbloc/test/riverbloc_test.dart
+++ packages/riverbloc/test/riverbloc_test.dart
@@ -33,6 +33,48 @@ class CounterCubit extends Cubit<int> {
 final counterCubitProvider = BlocProvider((ref) => CounterCubit(0));
 
 void main() {
+  group('Bloc test', () {
+    test(
+        'reads bloc with default state 0 and applies increments and decrements',
+        () async {
+      final container = ProviderContainer();
+
+      final counterBloc = container.read(counterBlocProvider);
+
+      expect(counterBloc.state, 0);
+
+      container.read(counterBlocProvider).add(CounterEvent.inc);
+      await Future(() {});
+      expect(counterBloc.state, 1);
+
+      container.read(counterBlocProvider).add(CounterEvent.inc);
+      container.read(counterBlocProvider).add(CounterEvent.inc);
+      await Future(() {});
+      expect(counterBloc.state, 3);
+
+      container.read(counterBlocProvider).add(CounterEvent.dec);
+      await Future(() {});
+      expect(counterBloc.state, 2);
+    });
+
+    test('defaults to 0 and notify listeners when value changes', () async {
+      final container = ProviderContainer();
+
+      final counterBloc = container.read(counterBlocProvider);
+
+      expect(counterBloc.state, 0);
+      expect(container.read(counterBlocProvider.state), 0);
+
+      for (var count = 0; count < 10; count++) {
+        container.read(counterBlocProvider).add(CounterEvent.inc);
+        expect(container.read(counterBlocProvider.state), count);
+        await Future(() {});
+        expect(counterBloc.state, count + 1);
+        expect(container.read(counterBlocProvider.state), count + 1);
+      }
+    });
+  });
+
   group('Cubit test', () {
     test('reads cubit with default state 0 and increments it', () {
       final container = ProviderContainer();
