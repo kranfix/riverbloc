8c21ae419c4ee6c3e4904c34b34235735386cd96
Frank Moreno <frankmoreno1993@gmail.com>
2021-03-18 12:42:57 -0500

    ordering imports

--- packages/flutter_hooks_bloc/lib/flutter_riverbloc.dart
+++ packages/flutter_hooks_bloc/lib/flutter_riverbloc.dart
@@ -1,11 +1,12 @@
 library flutter_riverbloc;
 
-export 'package:riverbloc/riverbloc.dart';
 export 'package:hooks_riverpod/hooks_riverpod.dart'
     show ProviderScope, Provider, BuildContextX;
-export 'src/bloc_hook.dart' show useBloc, useRiverBloc;
+export 'package:riverbloc/riverbloc.dart';
+
 export 'src/bloc_builder.dart';
+export 'src/bloc_consumer.dart';
+export 'src/bloc_hook.dart' show useBloc, useRiverBloc;
 export 'src/bloc_listener.dart'
     show BlocListener, BlocWidgetListener, BlocListenerCondition;
-export 'src/bloc_consumer.dart';
 export 'src/multi_bloc_listener.dart';
