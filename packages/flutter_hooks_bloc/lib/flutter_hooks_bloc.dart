fea3a83232abe8eb13469fa4eaf644e78007bf75
Frank Moreno <frankmoreno1993@gmail.com>
2021-03-18 14:18:05 -0500

    flutter_hooks_bloc: removing unused export

--- packages/flutter_hooks_bloc/lib/flutter_hooks_bloc.dart
+++ packages/flutter_hooks_bloc/lib/flutter_hooks_bloc.dart
@@ -2,7 +2,7 @@ library flutter_hooks_bloc;
 
 export 'src/bloc_builder.dart';
 export 'src/bloc_consumer.dart';
-export 'src/bloc_hook.dart' show useBloc, useRiverBloc;
+export 'src/bloc_hook.dart' show useBloc;
 export 'src/bloc_listener.dart'
     show BlocListener, BlocWidgetListener, BlocListenerCondition;
 export 'src/flutter_bloc.dart';
