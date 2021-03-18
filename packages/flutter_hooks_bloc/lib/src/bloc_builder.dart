eedd19a7b71cf46d9a24f53b17d32d40e35f3504
Frank Moreno <frankmoreno1993@gmail.com>
2021-03-18 13:47:56 -0500

    flutter_hooks_bloc: removing BlocBuilder.river

--- packages/flutter_hooks_bloc/lib/src/bloc_builder.dart
+++ packages/flutter_hooks_bloc/lib/src/bloc_builder.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/widgets.dart';
-import 'package:riverbloc/riverbloc.dart' show BlocProvider;
-
 import 'bloc_hook.dart';
 import 'flutter_bloc.dart' hide BlocProvider;
 
@@ -18,19 +16,17 @@ typedef BlocBuilderCondition<S> = bool Function(S previous, S current);
 /// Please refer to `BlocListener` if you want to "do" anything in response to
 /// `state` changes such as navigation, showing a dialog, etc...
 ///
-/// If the [bloc] parameter is omitted, [BlocBuilder] will automatically
-/// perform a lookup using [BlocProvider] and the current `BuildContext`.
 ///
 /// ```dart
 /// BlocBuilder<BlocA, BlocAState>(
 ///   builder: (context, state) {
-///   // return widget here based on BlocA's state
+///     // return widget here based on BlocA's state
 ///   }
 /// )
 /// ```
 ///
 /// Only specify the [bloc] if you wish to provide a [bloc] that is otherwise
-/// not accessible via [BlocProvider] and the current `BuildContext`.
+/// not accessible via the current `BuildContext`.
 ///
 /// ```dart
 /// BlocBuilder<BlocA, BlocAState>(
@@ -72,24 +68,11 @@ class BlocBuilder<B extends BlocBase<S>, S extends Object>
 
     /// The [bloc] that the [BlocBuilder] will interact with.
     /// If omitted, [BlocBuilder] will automatically perform a lookup using
-    /// [BlocProvider] and the current `BuildContext`.
+    /// the current `BuildContext`.
     B? bloc,
     required this.builder,
     this.buildWhen,
   }) : super(key: key, bloc: bloc);
-
-  /// Rebuilds its content based on a `riverbloc` provider.
-  @Deprecated(
-    'The BlocBuilder.river constructor is deprecated and'
-    ' will be removed on v0.14.0.'
-    'Use riverbloc instead',
-  )
-  const BlocBuilder.river({
-    Key? key,
-    required BlocProvider<B> provider,
-    required this.builder,
-    this.buildWhen,
-  }) : super.river(key: key, provider: provider);
 
   /// The [builder] function which will be invoked on each widget build.
   /// The [builder] takes the `BuildContext` and current `state` and
