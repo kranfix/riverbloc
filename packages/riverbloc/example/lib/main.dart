14f065c37015e8ac81fdc7f27e6415eb43677b68
Elian Ortega <elian.mortega@gmail.com>
2021-07-03 00:30:30 -0600

    Update example counter app

--- packages/riverbloc/example/lib/main.dart
+++ packages/riverbloc/example/lib/main.dart
@@ -39,10 +39,10 @@ class MyHomePage extends ConsumerWidget {
   final String title;
 
   @override
-  Widget build(BuildContext context, ScopedReader watch) {
+  Widget build(BuildContext context, WidgetRef ref) {
     // Rebuilds the widget if the cubit/bloc changes.
     // But does not rebuild if the state changes with the same cubit/bloc
-    final counterCubit = watch(counterProvider.notifier);
+    final counterCubit = ref.watch(counterProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
@@ -54,9 +54,9 @@ class MyHomePage extends ConsumerWidget {
             Text(
               'initial counterCubit.state: ${counterCubit.state}',
             ),
-            Consumer(builder: (context, watch, __) {
+            Consumer(builder: (context, ref, __) {
               // Rebuilds on every emitted state
-              final _counter = watch(counterProvider);
+              final _counter = ref.watch(counterProvider);
               return Text(
                 '$_counter',
                 style: Theme.of(context).textTheme.headline4,
@@ -66,7 +66,7 @@ class MyHomePage extends ConsumerWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => context.read(counterProvider.notifier).increment(),
+        onPressed: () => ref.read(counterProvider.notifier).increment(),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
