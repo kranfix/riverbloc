81125cfc0d4e02d02bb0fac44b491b1a72e89ae7
Frank Moreno <frankmoreno1993@gmail.com>
2021-08-29 22:43:55 -0500

    update for example

--- packages/riverbloc/example/lib/main.dart
+++ packages/riverbloc/example/lib/main.dart
@@ -62,6 +62,17 @@ class MyHomePage extends ConsumerWidget {
                 style: Theme.of(context).textTheme.headline4,
               );
             }),
+            Consumer(builder: (context, ref, __) {
+              final _counter = ref.watch(
+                counterProvider
+                    .when((prev, curr) => (curr + prev) % 5 == 0)
+                    .select((state) => 2 * state),
+              );
+              return Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            }),
           ],
         ),
       ),
