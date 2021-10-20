cc3620ff11071b22d93a2074edbecd27c1afcb19
Frank Moreno <frankmoreno1993@gmail.com>
2021-10-19 21:45:44 -0500

    fhb: RepositoryProvider passing original tests

--- packages/flutter_hooks_bloc/test/flutter_bloc_original/repository_provider_test.dart
+++ packages/flutter_hooks_bloc/test/flutter_bloc_original/repository_provider_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks_bloc/flutter_hooks_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 class MyApp extends StatelessWidget {
