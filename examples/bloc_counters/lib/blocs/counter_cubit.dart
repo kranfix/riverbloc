7361fe74b18a4f1d35fedd1d2592579735b95747
Frank Moreno <frankmoreno1993@gmail.com>
2021-10-28 14:08:39 -0500

    bloc_counters: cubit migrated to another directory

--- examples/bloc_counters/lib/blocs/counter_cubit.dart
+++ examples/bloc_counters/lib/blocs/counter_cubit.dart
@@ -3,8 +3,19 @@ import 'package:bloc/bloc.dart' show Cubit;
 class CounterCubitBase extends Cubit<int> {
   CounterCubitBase(int value) : super(value);
 
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
+  void increment([int value = 1]) async {
+    for (int i = 0; i < value; i++) {
+      emit(state + 1);
+      await Future.delayed(const Duration(seconds: 2));
+    }
+  }
+
+  void decrement([int value = 1]) async {
+    for (int i = 0; i < value; i++) {
+      emit(state - 1);
+      await Future.delayed(const Duration(seconds: 2));
+    }
+  }
 }
 
 mixin _CounterCubitActions on CounterCubitBase {}
