d5e5c1c9d15122503c03451d461ea69d400db8a4
Frank Moreno <frankmoreno1993@gmail.com>
2021-11-25 07:09:07 -0500

    library docs

--- packages/riverbloc/lib/riverbloc.dart
+++ packages/riverbloc/lib/riverbloc.dart
@@ -1,4 +1,15 @@
+/// Riverbloc exposes providers for [Bloc] and [Cubit] instances bases in
+/// `riverpod` package instead of `provider` package.
+///
+/// Providers:
+/// - [BlocProvider]
+/// - [AutoDisposeBlocProvider]
+/// - [BlocProviderFamily]
+/// - [AutoDisposeBlocProviderFamily]
 library riverbloc;
+
+import 'package:bloc/bloc.dart';
+import 'package:riverbloc/src/framework.dart';
 
 export 'package:bloc/bloc.dart';
 export 'package:riverpod/riverpod.dart';
